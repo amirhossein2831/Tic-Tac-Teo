5790df4501fd785905649db1ce9a89ae9c539741
Amirhossein <Amirmemool12@gmail.com>
2023-06-21 16:44:05 -0400

    create Application program class

---
+++ src/main/java/com/example/game/Application.java
@@ -0,0 +1,28 @@
+package com.example.game;
+
+import com.example.game.Media.MediaPlayer;
+import com.example.game.Util.Util;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("application.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 858, 590);
+        stage.setTitle("Tic Tac Toe");
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        MediaPlayer.changeMusic(Util.MUSIC_PATH);
+        launch();
+    }
+}
